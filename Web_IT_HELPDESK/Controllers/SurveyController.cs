19fbad12e679d0eeea09c19cae7bcb3bf7e70241
sangnm-w <sangnm.w@gmail.com>
2020-11-19 22:09:46 +0700

    CanteenSurvey

--- Web_IT_HELPDESK/Controllers/SurveyController.cs
+++ Web_IT_HELPDESK/Controllers/SurveyController.cs
@@ -80,9 +80,11 @@ namespace Web_IT_HELPDESK.Controllers
 
         // GET: Survey/CanteenSurvey
         [Authorize]
-        public ActionResult CanteenSurvey(Guid? id)
+        public ActionResult CanteenSurvey()
         {
-            return View();
+            var questions = db.QUESTIONs.Where(i => i.DEL != true && i.NOTE == "CanteenSurvey");
+            ViewBag.Switch = 7;
+            return View(questions);
         }
 
         // POST: Survey/CanteenSurvey
@@ -92,7 +94,7 @@ namespace Web_IT_HELPDESK.Controllers
         public ActionResult CanteenSurvey(IEnumerable<string> answertexts, IEnumerable<string> answerRadios)
         {
             bool result = true;
-            int maxQuestionNo = db.QUESTIONs.Where(q => q.NOTE == "SecuritySurvey").Count();
+            int maxQuestionNo = db.QUESTIONs.Where(q => q.NOTE == "CanteenSurvey").Count();
             int radioNo = 0;
             int textNo = 0;
 
@@ -103,13 +105,13 @@ namespace Web_IT_HELPDESK.Controllers
                 EMP_ANSWER emp_answer = new EMP_ANSWER();
 
                 emp_answer.ID = Guid.NewGuid();
-                emp_answer.QUESTION_ID = db.QUESTIONs.FirstOrDefault(q => q.No == questionNo && q.NOTE == "SecuritySurvey").ID;
+                emp_answer.QUESTION_ID = db.QUESTIONs.FirstOrDefault(q => q.No == questionNo && q.NOTE == "CanteenSurvey").ID;
                 emp_answer.EMPLOYEEID = CurrentUser.Instance.User.Emp_CJ;
                 emp_answer.DATE = DateTime.Now;
 
-                if (questionNo == 0)
+                if (questionNo < 0) //exclude (question no need answer) 1/3/4/8
                     emp_answer.NOTE = null;
-                else if (questionNo <= 16)
+                else if (questionNo < 7)
                 {
                     emp_answer.NOTE = answerRadios.ElementAt(radioNo).ToString();
                     radioNo++;
