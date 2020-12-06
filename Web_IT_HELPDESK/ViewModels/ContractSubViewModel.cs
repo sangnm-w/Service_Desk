049d6625afaf450a919eb7121d463f81552878ca
sangnm-w <sangnm.w@gmail.com>
2020-12-06 14:28:01 +0700

    Waiting for new idea

---
+++ Web_IT_HELPDESK/ViewModels/ContractSubViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web_IT_HELPDESK.ViewModels
+{
+    public class ContractSubViewModel
+    {
+        public CONTRACT_SUB ContractSub { get; set; }
+        [Required(ErrorMessage = "Á à!")]
+        public HttpPostedFileBase ContentSubFile { get; set; }
+    }
+}
